52eee4ec5fd8c83825b84026b344cc20b23cf5e7
Vetrichelvan <pythonhub.py@gmail.com>
2022-10-07 00:25:09 +0530

    Added options to dynamically generate a flask or fast app also handled some exceptions

--- faag_cli/faag.py
+++ faag_cli/faag.py
@@ -1,19 +1,33 @@
 import os
+import sys
 
-from typer import run
+import typer
+from typer import Typer, Option
 
 from faag_cli.constants import FOLDERS_FILES
 from faag_cli.utils import templates_environment
 
+typer_app = Typer()
+
 
 class Faag:
     @staticmethod
-    def generate_app() -> None:
+    def generate_app(app, app_name: str) -> None:
+        if os.path.exists("app"):
+            typer.echo("App already exists")
+            sys.exit(1)
+        if app not in ["fast_api", "flask"]:
+            raise ValueError(
+                "Invalid app type. Please provide valid arguments [fast_api, flask]"
+            )
+        if app == "flask":
+            print("Generating flask app is currently under development. Please wait")
+            sys.exit(1)
         os.mkdir("app")
-        fast_template = templates_environment.get_template("fast_api__init__.jinja")
-        fast_template_rendered = fast_template.render()
+        app_template = templates_environment.get_template(f"{app}__init__.jinja")
+        app_template_rendered = app_template.render(app_name=app_name)
         with open("app/__init__.py", "w", encoding="UTF-8") as fast_api_init:
-            fast_api_init.write(fast_template_rendered)
+            fast_api_init.write(app_template_rendered)
 
     @staticmethod
     def add_gitignore():
@@ -42,13 +56,25 @@ class Faag:
                     gen_file.write(rendered_template)
 
 
-def faag_cli():
+@typer_app.command(
+    name="Faag_CLI",
+    help="Generate a new FastAPI/Flask project",
+)
+def generate(
+    app_type: str = Option(
+        "fast_api",
+        "--app",
+        "-a",
+        help="Type of that should be generated. Default type is fast_api. Valid Options are: [fast_api, flask]",
+    ),
+    app_name: str = Option("sampel_app", "--app-name", "-an", help="Name of the app"),
+) -> None:
     faag = Faag()
     faag.add_gitignore()
-    faag.generate_app()
+    faag.generate_app(app_type, app_name)
     faag.add_folders_files()
     faag.add_packages()
 
 
 if __name__ == "__main__":
-    run(faag_cli)
+    typer_app()
