4a0f135d8eb3f655b7034e327ecdb054915bfd2e
Vetrichelvan <pythonhub.py@gmail.com>
2022-10-10 00:11:41 +0530

    Added app_name validation

--- faag_cli/utils/faag_utils.py
+++ faag_cli/utils/faag_utils.py
@@ -1,5 +1,8 @@
 import os
+import re
 import sys
+from re import Pattern
+
 from rich import print as rprint
 
 from faag_cli.utils.templates_loader import templates_environment
@@ -20,3 +23,18 @@ class FaagUtils:
                 "[bold red]:police_car_light:Error: App already exists. Please delete the app folder and try again[/bold red]"
             )
             sys.exit(1)
+
+    @staticmethod
+    def validate_app_name(app_name: str) -> str:
+        app_name_pattern: Pattern = re.compile(r".*[@!#$%^&*()<>?/\|}{~:].*")
+        if app_name_pattern.match(app_name):
+            rprint(
+                "[bold red]:police_car_light:Error: App name should not contain special characters[/bold red]"
+            )
+            sys.exit(1)
+        if app_name[0].isdigit():
+            rprint(
+                "[bold red]:police_car_light:Error: App name should not start with a number[/bold red]"
+            )
+            sys.exit(1)
+        return app_name.replace("-", "_")
